9a70fc49af2f1f3a099bd34412942806fab98cd7
Henrique da Silva <hpsilva@pm.me>
2024-06-24 18:03:56 -0300

    .

--- Scenes/Characters/Enemy/EnemyPatrolState.cs
+++ Scenes/Characters/Enemy/EnemyPatrolState.cs
@@ -3,16 +3,31 @@ using System;
 
 public partial class EnemyPatrolState : EnemyState
 {
+    private int pointIndex;
     protected override void EnterState()
     {
-        characterNode!.AnimationPlayerNode!.Play(AC.EnemyAnimation.Move.ToString());
 
-        destination = GetPointGlobalPosition(1);
+        characterNode!.AnimationPlayerNode!.Play(AC.EnemyAnimation.Move.ToString());
+        pointIndex = 1;
+        destination = GetPointGlobalPosition(pointIndex);
         characterNode.AgentNode.TargetPosition = destination;
+
+        characterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
     }
+
 
     public sealed override void _PhysicsProcess(double delta)
     {
         Move();
+    }
+
+    private void HandleNavigationFinished()
+    {
+        pointIndex = Mathf.Wrap(
+            pointIndex + 1, 0, characterNode.pathNode.Curve.PointCount
+        );
+
+        destination = GetPointGlobalPosition(pointIndex);
+        characterNode.AgentNode.TargetPosition = destination;
     }
 }
